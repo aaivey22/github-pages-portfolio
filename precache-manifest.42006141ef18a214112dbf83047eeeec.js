fb5e47564946a0b8fee8fea0254d73acd70bcb54
aaivey22 <angelawings22@gmail.com>
2020-07-28 12:17:16 -0400

    Updates

---
+++ precache-manifest.42006141ef18a214112dbf83047eeeec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87032853e5374979f1ae51de8d8c490",
+    "url": "/github-pages-portfolio/index.html"
+  },
+  {
+    "revision": "d374c732ded673c0812a",
+    "url": "/github-pages-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2d6559003750daafe84",
+    "url": "/github-pages-portfolio/static/js/2.191657b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-portfolio/static/js/2.191657b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d374c732ded673c0812a",
+    "url": "/github-pages-portfolio/static/js/main.25c50183.chunk.js"
+  },
+  {
+    "revision": "3c9d40f105d8b41c7c4e",
+    "url": "/github-pages-portfolio/static/js/runtime-main.5e463924.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
